d1f9a136d35987fc1a8923094d586c3e335ed190
Wei Guo <me@imkira.com>
2019-08-31 21:45:36 +0800

    disable bitcode (patch from #32963)

--- app/darwin_ios.m
+++ app/darwin_ios.m
@@ -135,8 +135,9 @@ static void sendTouches(int change, NSSet* touches) {
 @end
 
 void runApp(void) {
+	char * argv[] = {};
 	@autoreleasepool {
-		UIApplicationMain(0, nil, nil, NSStringFromClass([GoAppAppDelegate class]));
+		UIApplicationMain(0, argv, nil, NSStringFromClass([GoAppAppDelegate class]));
 	}
 }
 
